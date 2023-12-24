243f6e67058f48cab115ec4dd38fe13f74437662
tdisk6 <137001331+tdisk6@users.noreply.github.com>
2023-12-24 21:29:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '24706898'))
 API_HASH = environ.get('API_HASH', 'f0cbf6f1b141fa20b9269908e453fa23')
-BOT_TOKEN = environ.get('BOT_TOKEN', '5541164881:AAFbyROJbpEvfERf-e143y_qjezC5NKzQfY')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6956983789:AAHxlvHyUyVB6Bfm_DKpcMhUZjHppYdnQ7Y')
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
